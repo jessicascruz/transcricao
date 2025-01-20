d9d1511259d55a3334e8d1b3e49a7ef3d76b39c1
Jéssica Cruz <jessica.santos648@gmail.com>
2025-01-20 16:21:40 -0300

    criação da base

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # transcricao
+
+terminal:
+fastapi dev main.py
+
+Swagger
+http://127.0.0.1:8000/docs
+
+uvicorn main:app --host 0.0.0.0 --port 80
+uvicorn main:app --reload => comando para rodar servidor
---
+++ backend/main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI, HTTPException, Depends
+from typing import Optional, List, Dict
+from pydantic import BaseModel
+from uuid import UUID, uuid4
+from fastapi.middleware.cors import CORSMiddleware # import here
+
+app = FastAPI()
+
+# declare origin/s
+origins = [
+    "http://localhost:5173",
+    "localhost:5173"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
